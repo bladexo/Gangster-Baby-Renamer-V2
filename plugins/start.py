6391efc554a7d9c19a83072a1d0ae7f975de02bd
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-11-15 01:26:58 +0100

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,9 +123,9 @@ async def send_doc(client, message):
     except:
         await message.reply_text(text=f"Hello dear {message.from_user.first_name}  **we are currently working on this issue**\n\nPlease try to rename files from your another account.\nBecause this BOT can't rename file sent by some ids.\n\nIf you are an **ADMIN** Don't worry ! here we have a solution for you dear {message.from_user.first_name }.\n\nPlease use \n👉 `/addpremium your_other_userid` 👈 to use premium feautres\n\n",
                                   reply_markup=InlineKeyboardMarkup([
-                                                                     [InlineKeyboardButton("🦋 Contact LazyDeveloper 🦋", url='https://telegram.me/MR_DINNO1')],
+                                                                     [[InlineKeyboardButton("🦋 Contact LazyDeveloper 🦋", url='https://telegram.me/MR_DINNO1')],
                                                                      [InlineKeyboardButton("🔺 Watch Tutorial 🔺", url='https://youtube.com/@LazyDevelperr')],
-                                                                     [InlineKeyboardButton("🦋 Visit Channel  ", url='https://t.me/RedOfficiall')],
+                                                                     InlineKeyboardButton("🦋 Visit Channel  ", url='https://t.me/RedOfficiall')],
                                                                      [InlineKeyboardButton("☕ Buy Me A Coffee ☕", url='https://buymeacoffee.com/redofficial')]
                                                                     ))
         await message.reply_text(text=f"🦋")
