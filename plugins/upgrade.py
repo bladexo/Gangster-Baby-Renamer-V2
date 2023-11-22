e6b0e293a32326a4c40f010b8b063759f24eb007
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-11-22 20:15:22 +0100

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -4,31 +4,12 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-	text = """**Free Plan User**
-	Daily  Upload limit 1.2GB
-	Price 0
-	
-	**🪙 Silver Tier 🪙** 
-	Daily  Upload  limit 10GB
-	Price Rs 66  ind /🌎 0.8$  per Month
-	
-	**💫 Gold Tier 💫**
-	Daily Upload limit 50GB
-	Price Rs 100  ind /🌎 1.2$  per Month
-	
-	**💎 Diamond 💎**
-	Daily Upload limit 100GB
-	Price Rs 206  ind /🌎 2.5$  per Month
-	
-	
-	Pay Using Upi I'd ```7808912076@paytm```
-	
-	After Payment Send Screenshots Of 
-        Payment To Admin @mRiderDM"""
+	text = """**PREMIIUM PLAN NOT AVAILABLE T THE MOMENT**
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/MR_DINNO1")], 
         			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("Cancel",callback_data = "cancel") 
+	   ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
